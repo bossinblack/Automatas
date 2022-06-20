6206aabfafd0f6c866d9eb95d0e24f48df1feca3
Daiana Ascurra <dayurra1989@gmail.com>
2022-06-20 17:59:52 -0300

    Agrego desarrollo de Trabajo en Clases3 hecho por mi

---
+++ TC3/practica/TC3DAY/EJERCICIO1DAY.py
@@ -0,0 +1,14 @@
+import re
+regex = "^[aAbB]*$"
+
+cadena = input("Ingrese cadena: ")
+isMatch = re.match(regex, cadena)
+if not isMatch:
+    print("Cadena invalida. ")
+else:
+    print("------Estado inicial A: cadena ingresada------", cadena)
+    for i in list(cadena):
+        if i == 'a' or i == 'A':
+            print("Con %s Pasa a estado B" % i)
+        elif i == 'b' or i == 'B':
+            print("Con %s Pasa a estado C" % i)
